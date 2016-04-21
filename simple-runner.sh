64bb9f346cc06b2d224021c03f33464cc170f41c
Dan Fabulich <dan.fabulich@redfin.com>
2016-04-20 21:36:18 -0700

    Workaround https://github.com/bazelbuild/bazel/issues/1147, fix working directory
    The working directory under Skylark is bazel-skylark-depends-on-java-binary, but the script expects the working directory to be bazel-bin/simple-runner.runfiles

--- simple-runner.sh
+++ simple-runner.sh
@@ -1,1 +1,8 @@
-./my-other-runner > $1
+#!/bin/bash -eux
+START_DIR=`pwd`
+OUTPUT=$1
+if [ "${OUTPUT:0:1}" != "/" ]; then
+        OUTPUT=$START_DIR/$1
+fi
+cd ${BASH_SOURCE[0]}.runfiles
+./my-other-runner > $OUTPUT
